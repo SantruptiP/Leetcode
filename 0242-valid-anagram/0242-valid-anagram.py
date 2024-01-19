e111dcbf7656847bd6974d724a388c99f80f7ffd
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2024-01-19 17:33:51 +0530

    Time: 59 ms (30.74%), Space: 17.5 MB (54.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        res1 = ''.join(sorted(s))
-        res2 = ''.join(sorted(t))
-        if res1==res2:
-            return True
-        else:
-            return False
+        return sorted(s)==sorted(t)
