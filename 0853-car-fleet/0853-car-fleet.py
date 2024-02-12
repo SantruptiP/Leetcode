d1714695e4e32b55207e97c02f20f6d4e145172f
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2024-02-12 16:17:32 +0530

    Time: 706 ms (60.12%), Space: 39.1 MB (50.10%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -4,7 +4,7 @@ class Solution:
         pair.sort(reverse=True) # Sort in reverse order
         stack = []
         for p, s in pair:  # Reverse Sorted Order
-            stack.append((target - p) / s) # append time taken to reach destination to stack
+            stack.append((target - p) / s) # Append time taken to reach destination to stack
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop() 
+                stack.pop() # If the time taken by the next car is less then they will collide                               so can pop that car and consider it one fleet
         return len(stack)
